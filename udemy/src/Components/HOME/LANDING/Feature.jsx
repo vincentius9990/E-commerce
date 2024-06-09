bfeb376c14f56abc779c02c56b168f253827f0ca
vincent <vincentrkumar98@gmail.com>
2024-06-09 20:07:18 +0530

    changes

--- udemy/src/Components/HOME/LANDING/Feature.jsx
+++ udemy/src/Components/HOME/LANDING/Feature.jsx
@@ -3,11 +3,11 @@ import './feature.css'
 import Card from './MYCard'
 const Feature = () => {
   
-  const img1='src/Components/IMAGES/buildquality.jpg';
+  const img1='udemy/public/IMAGES/buildquality.jpg';
   const img2='src/Components/IMAGES/batterylife.jpg';
   const img3='src/Components/IMAGES/qualcomm-snapdragon-w5.webp';
   const img4='src/Components/IMAGES/Smart-Watch-Battery-Saving-Tips-Gear.jpg';
-
+  // buildquality.jpg
   
     return (
     <>
