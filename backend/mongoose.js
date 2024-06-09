d4bf84900eaaa118fe654280cd4d08f3cfed3af2
vincent <vincentrkumar98@gmail.com>
2024-06-09 22:49:22 +0530

    changes done

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-mongoose.connect("mongodb://localhost:27017/E-Commerce",{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/E-Commerce",{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
 console.log("database connection successfull");
 
 }).catch((err)=>{
