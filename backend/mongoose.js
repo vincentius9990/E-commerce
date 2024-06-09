43f29bdc0e2b1fbbbf2b3cd5d7fe1794fd537cde
vincent <vincentrkumar98@gmail.com>
2024-06-10 00:03:07 +0530

    mongoose.js changes done

--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose =require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/E-Commerce").then(()=>{
+require('dotenv').config();
+mongoose.connect(process.env._mongoose).then(()=>{
 console.log("database connection successfull");
 
 }).catch((err)=>{
